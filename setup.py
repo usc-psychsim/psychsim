56a24d616f9d6e28216e227994f2253171495b1b
Brett Israelsen <brett.israelsen@gmail.com>
2020-02-21 16:56:09 -0500

    making module into a package

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="psychsim",
+    version="1.0",
+    author="David Pynadath",
+    author_email="pynadath@ict.usc.edu",
+    description="PsychSim is an open-source social-simulation framework using decision-theoretic agents who have a theory of mind",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/usc-psychsim/atomic",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
