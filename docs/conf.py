9205c2b9d97cb5f784e5e804b6400ab213614d4a
David V. Pynadath <david@pynadath.net>
2021-07-19 00:00:41 -0700

    Point RtD to index

--- docs/conf.py
+++ docs/conf.py
@@ -33,6 +33,9 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.githubpages'
 ]
 
+# The master toctree document.
+master_doc = 'index'
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
