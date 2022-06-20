c0e9e3da28f15b9087217a27eabe84dc22df6ea2
David V. Pynadath <david@pynadath.net>
2022-06-19 22:59:06 -0700

    Fixed module path

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../psychsim'))
+sys.path.insert(0, os.path.abspath('..'))
 
 
 # -- Project information -----------------------------------------------------
