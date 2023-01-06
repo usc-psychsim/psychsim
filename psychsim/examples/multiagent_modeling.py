6f63a19f21e249fd2e66d61cf3f1b8aacbbd7a39
Pedro Sequeira <pedrodbs@gmail.com>
2023-01-06 00:46:25 -0800

    updated multiagent_modeling example

--- psychsim/examples/multiagent_modeling.py
+++ psychsim/examples/multiagent_modeling.py
@@ -3,17 +3,17 @@ import time
 
 from psychsim.agent import Agent
 from psychsim.probability import Distribution
-from psychsim.world import World
 from psychsim.pwl import makeTree, modelKey, incrementMatrix, setToConstantMatrix, rewardKey, stateKey
 from psychsim.reward import maximizeFeature, minimizeFeature
+from psychsim.world import World
 
 __author__ = 'Pedro Sequeira'
 __email__ = 'pedrodbs@gmail.com'
 __description__ = 'Example of inference over reward models of another agent based on observations of their behavior.' \
-                  'Two agents (actor1 and action2) moves in the environment (always right or left).' \
-                  'First agent (actor1) maintains a belief over the other actor\'s reward model (either an agent ' \
-                  'that prefers going right, going left or acting randomly. This belief is updated based on ' \
-                  'observations of the actor\'s actions over time.'
+                  'Two agents (actor1 and action2) move in the environment (always right or left).' \
+                  'Each agent maintains a belief over the other actor\'s reward model (either an agent ' \
+                  'that prefers going right, going left or acting randomly). The belief is updated based on ' \
+                  'observations of the other actor\'s actions over time.'
 
 # parameters
 MAX_STEPS = 5
@@ -21,6 +21,7 @@ AGENT_NAMES = ['actor1', 'actor2']
 HORIZON = 2
 AGENT_SELECTION = 'random'
 MODEL_RATIONALITY = 0.5
+MODEL_SELECTION = 'softmax'
 SEED = 17
 
 
@@ -44,7 +45,7 @@ if __name__ == '__main__':
         agent.setAttribute('selection', AGENT_SELECTION)
 
         # add location variable
-        loc = world.defineState(agent.name, 'location', int, -1000, 1000, 'Agent\'s location')
+        loc = world.defineState(agent.name, 'location', int, -1000, 1000, description='Agent\'s location')
         world.setFeature(loc, 0)
 
         # define agents' actions (left and right)
@@ -64,64 +65,77 @@ if __name__ == '__main__':
         agent.setAttribute('rationality', MODEL_RATIONALITY)
         agent.setAttribute('selection', 'random')  # also set selection to distribution
         agent.setAttribute('horizon', HORIZON)
-        agent.setAttribute('beliefs', True)
 
         team.append(agent)
 
     world.setOrder([{agent.name for agent in team}])
 
-    learner_agent = team[0]  # actor1 is modeling actor2
-    for ag_i, agent in enumerate(team):
-        if agent.name != learner_agent.name:  # create models for actor2
-            # add agent models (prefer positive vs negative location vs random agent)
-            true_model = agent.get_true_model()
-            loc = stateKey(agent.name, 'location')
+    # add agent models (prefer positive vs negative location vs random agent)
+    for agent in team:
+        agent.create_belief_state()
+        agent.set_observations()  # agent does not observe other agent's true model
 
-            prefer_pos_model = f'{agent.name}_' + 'prefer_positive_loc'
-            agent.addModel(prefer_pos_model, parent=true_model)
-            agent.setReward(maximizeFeature(loc, agent.name), 1., model=prefer_pos_model)
+        true_model = agent.get_true_model()
+        loc = stateKey(agent.name, 'location')
 
-            prefer_neg_model = f'{agent.name}_' + 'prefer_negative_loc'
-            agent.addModel(prefer_neg_model, parent=true_model)
-            agent.setReward(minimizeFeature(loc, agent.name), 1., model=prefer_neg_model)
+        prefer_pos_model = f'{agent.name}_' + 'prefer_positive_loc'
+        agent.addModel(prefer_pos_model, parent=true_model)
+        agent.setReward(maximizeFeature(loc, agent.name), 1., model=prefer_pos_model)
 
-            prefer_nothing = f'{agent.name}_' + 'prefer_nothing'  # random agent
-            agent.addModel(prefer_nothing, parent=true_model)
-            agent.setReward(setToConstantMatrix(rewardKey(agent.name), 0.), 1., model=prefer_nothing)
+        prefer_neg_model = f'{agent.name}_' + 'prefer_negative_loc'
+        agent.addModel(prefer_neg_model, parent=true_model)
+        agent.setReward(minimizeFeature(loc, agent.name), 1., model=prefer_neg_model)
 
-            # null_model = agent.zero_level(sample=True)  # TODO this seems to lead to a leak into the agent's true model?
+        prefer_nothing = f'{agent.name}_' + 'prefer_nothing'  # random agent
+        agent.addModel(prefer_nothing, parent=true_model)
+        agent.setReward(setToConstantMatrix(rewardKey(agent.name), 0.), 1., model=prefer_nothing)
 
-            # set uniform belief over agent's model in actor1
-            model_names = [name for name in agent.models.keys() if name != true_model]
-            dist = Distribution({model: 1. / (len(agent.models) - 1) for model in model_names})
-            world.setMentalModel(learner_agent.name, agent.name, dist)
+        model_names = [name for name in agent.models.keys() if name != true_model]
+        for model in model_names:
+            # make models less rational to get smoother (more cautious) inference
+            agent.setAttribute('rationality', MODEL_RATIONALITY, model=model)
+            agent.setAttribute('selection', MODEL_SELECTION, model=model)  # also set selection to distribution
+            agent.setAttribute('horizon', HORIZON, model=model)
+            agent.create_belief_state(model=model)
 
-            for model in model_names:
-                # make models less rational to get smoother (more cautious) inference
-                agent.setAttribute('rationality', MODEL_RATIONALITY, model=model)
-                agent.setAttribute('selection', 'distribution', model=model)  # also set selection to distribution
-                agent.setAttribute('horizon', HORIZON, model=model)
-                agent.setAttribute('beliefs', True, model=model)
+    # set mental models
+    for agent in team:
+        other = team[1] if agent is team[0] else team[0]
 
-    # actor1 updates belief based on observations
-    # learner_agent.set_observations()
+        true_model = other.get_true_model()
+        model_names = [name for name in other.models.keys() if name != true_model and 'zero' not in name]
+
+        # set uniform belief over other agent's model
+        dist = Distribution({model: 1. for model in model_names})
+        dist.normalize()
+        world.setMentalModel(agent.name, other.name, dist)
+
+        # set zero level agent to all other agent's models to avoid recursion
+        zero_level = agent.zero_level(sample=True, horizon=0)
+        for model in model_names:
+            world.setMentalModel(other.name, agent.name, zero_level, model=model)
 
     for agent in team:
-        agent_model = modelKey(agent.name)
+        other = team[1] if agent is team[0] else team[0]
+        other_model = modelKey(other.name)
         loc = stateKey(agent.name, 'location')
         print('====================================')
         print(f'Initial {agent.name} loc: {world.getFeature(loc)}')
-        print(f'Initial belief about {agent.name}\'s model:\n{_get_belief(agent_model, learner_agent)}')
+        print(f'Initial belief about {other.name}\'s model:\n{_get_belief(other_model, agent)}')
 
     start_time = time.time()
     for i in range(MAX_STEPS):
         print('====================================')
         print('Step:', i)
         step = world.step(select=False, horizon=HORIZON, tiebreak='distribution')
-        for agent in [team[1]]:
-            agent_model = modelKey(agent.name)
+
+        for agent in team:
+            other = team[1] if agent is team[0] else team[0]
+            other_model = modelKey(other.name)
             loc = stateKey(agent.name, 'location')
+            print('====================================')
             print(f'Current {agent.name} loc: {world.getFeature(loc)}')
-            print(f'Updated belief about {agent.name}\'s model:\n{_get_belief(agent_model, learner_agent)}')
-        print(f'Time spent: {time.time()-start_time: .2f}s')
+            print(f'Current belief about {other.name}\'s model:\n{_get_belief(other_model, agent)}')
+
+        print(f'Time spent: {time.time() - start_time: .2f}s')
         start_time = time.time()
